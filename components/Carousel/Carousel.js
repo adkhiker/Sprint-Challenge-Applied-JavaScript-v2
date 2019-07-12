3bfeb0e3283a302f8dce799d4076e76fc10f643d
adkhiker <poirier.greg@gmail.com>
2019-07-12 14:20:17 -0400

    Added automatic 3 second image advance on an infinate loop for the carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,4 +1,7 @@
-var slideIndex = 1;
+
+//slideshow manual advance
+
+/*  var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -27,4 +30,22 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}  */
+
+//Automatic slideshow advance: 3 seconds for each slide
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
